fdf8cd1da1a31a72a2b0cc8f05cdbd1be0420818
YasinduTharanga <yasindupremathilake07@gmail.com>
2024-06-22 22:22:46 +0530

    initial commit

---
+++ JWTAuthenticationWebAPI/Controllers/LoginController.cs
@@ -0,0 +1,27 @@
+﻿using JWTAuthenticationWebAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JWTAuthenticationWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        private IConfiguration _config;
+        public LoginController(IConfiguration configuration)
+        {
+            _config = configuration;
+        }
+
+        private Users AuthenticateUser(Users user)
+        {
+            Users _user = null;
+            if (user.Username == "admin" && user.Password == "12345")
+            {
+                _user = new Users { Username = "Yasindu Tharanga"};
+            }
+            return _user;
+        }
+    }
+}
